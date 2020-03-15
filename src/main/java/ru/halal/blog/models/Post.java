0ad906396e0845764eb98867acfd58cebca3d531
Almir <almir.almiev@tatar.ru>
2020-03-15 21:21:07 +0300

    Added Post model

---
+++ src/main/java/ru/halal/blog/models/Post.java
@@ -0,0 +1,58 @@
+package ru.halal.blog.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String full_text;
+    private String anonce;
+    private String title;
+    private int views;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFull_text() {
+        return full_text;
+    }
+
+    public void setFull_text(String full_text) {
+        this.full_text = full_text;
+    }
+
+    public String getAnonce() {
+        return anonce;
+    }
+
+    public void setAnonce(String anonce) {
+        this.anonce = anonce;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+}
